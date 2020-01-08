a44c93546615f57daccbdd4d38ddc95f7366acc1
DESKTOP-R7E62OP\Christian L'Orange <lorange.christian@gmail.com>
2020-01-08 11:06:09 -0700

    Adding admob

--- js/admob.js
+++ js/admob.js
@@ -1,13 +1,13 @@
 var admobid = {}
 if (/(android)/i.test(navigator.userAgent)) {  // for android & amazon-fireos
   admobid = {
-    banner: 'ca-app-pub-7061349157136298/2581148651',
-    interstitial: 'ca-app-pub-7061349157136298/5539194924',
+    banner: 'ca-app-pub-7061349157136298/2172922299',
+    interstitial: 'ca-app-pub-7061349157136298/1316982108',
   }
 } else if (/(ipod|iphone|ipad)/i.test(navigator.userAgent)) {  // for ios
   admobid = {
-    banner: 'ca-app-pub-7061349157136298/8712153170',
-    interstitial: 'ca-app-pub-7061349157136298/1955173135',
+    banner: 'ca-app-pub-7061349157136298/5729023927',
+    interstitial: 'ca-app-pub-7061349157136298/8163615571',
   }
 }
 
