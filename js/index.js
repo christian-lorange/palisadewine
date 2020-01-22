3c3f598eb8f47b36dd0cdc7e9fc205cb573cf40d
DESKTOP-R7E62OP\Christian L'Orange <lorange.christian@gmail.com>
2020-01-22 09:32:06 -0700

    no message

--- js/index.js
+++ js/index.js
@@ -19,6 +19,9 @@ function route() {
   var x = document.getElementById("route");
   x.style.display = "block";
 
+
+  ga_anl('send', 'pageview', {'page': 'Route.html'});  
+
 }
 
 function tour() {
@@ -28,7 +31,17 @@ function tour() {
   });
   var x = document.getElementById("tour");
   x.style.display = "block";
+  ga_anl('send', 'pageview', {'page': 'Tour.html'});  
 
+}
+
+function about() {
+  // moveTo( ".main", 1 );
+  [].forEach.call(document.querySelectorAll('.major_section'), function (el) {
+  el.style.display = 'none';
+  });
+  var x = document.getElementById("about");
+  x.style.display = "block";
 }
 
 function mapload() {
